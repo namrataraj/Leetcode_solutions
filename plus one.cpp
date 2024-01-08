bfac87b8ee20dcd293b1fc526b2c25496d7d7fd7
Namrata Raj <120201079+namrataraj@users.noreply.github.com>
2024-01-09 00:10:08 +0530

    Add files via upload

---
+++ plus one.cpp
@@ -0,0 +1,25 @@
+/*You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits*/
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for(int i = n-1; i >= 0; i--){
+            if(i == n-1)
+                digits[i]++;
+            if(digits[i] == 10){
+                digits[i] = 0;
+                if(i != 0){
+                    digits[i-1]++;
+                }
+                else{
+                    digits.push_back(0);
+                    digits[i] = 1;
+                }
+            }
+        }
+        return digits;
+        
+    }
+};
